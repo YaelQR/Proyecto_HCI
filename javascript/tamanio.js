f0693b2a3680b40db7af1a1a9c7374cfdb5baa70
Yael Quintanilla <yael.quintanilla.rmz2@outlook.com>
2024-11-18 22:09:22 -0600

    Se cambiao el nav de la pantalla de publicacion

--- javascript/tamanio.js
+++ javascript/tamanio.js
@@ -1,6 +1,8 @@
 function size(){
 
-    var div=document.getElementById('main');
-    div.style.height = window.innerHeight+'px';
+    //var div=document.getElementById('main');
+    var div=document.getElementById('body');
+    //div.style.height = window.innerHeight+'px';
+    div.style.width = window.innerWidth+'px';
     
 }
